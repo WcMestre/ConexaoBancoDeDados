97e66af5b8c06de83e66ba9cb78a1547fffc1777
Wendel Côrtes Martins <wcmestre@hotmail.com>
2021-02-17 14:18:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConexaoBancoDeDados/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace ConexaoBancoDeDados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtview = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtview
+            // 
+            this.dtview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtview.Location = new System.Drawing.Point(12, 12);
+            this.dtview.Name = "dtview";
+            this.dtview.Size = new System.Drawing.Size(776, 426);
+            this.dtview.TabIndex = 0;
+            this.dtview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtview_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtview);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtview;
+    }
+}
+
---
+++ ConexaoBancoDeDados/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ConexaoBancoDeDados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection1 = new SqlConnection("Server=179.108.180.209;Database=wendel;User Id=sa;Password=senac123;");
+            
+            sqlConnection1.Open();
+
+            //criando o select e o objeto de consulta
+            string sql = "SELECT * FROM CIDADES";
+            SqlCommand cmd = new SqlCommand(sql, sqlConnection1);
+            cmd.Connection = sqlConnection1;
+            cmd.CommandText = sql;
+
+            // cria o dataadapter...
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+
+            // preenche o dataset...
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+
+            dtview.DataSource = dataSet;
+            dtview.DataMember = dataSet.Tables[0].TableName;
+         
+            sqlConnection1.Close();            
+        }
+
+        private void dtview_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
