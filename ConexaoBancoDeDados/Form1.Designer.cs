3b10d2ce58adb7fd0d84c436672204b7273fec55
Wendel Côrtes Martins <wcmestre@hotmail.com>
2021-02-19 14:18:33 -0300

    salvar

--- ConexaoBancoDeDados/Form1.Designer.cs
+++ ConexaoBancoDeDados/Form1.Designer.cs
@@ -30,35 +30,111 @@ namespace ConexaoBancoDeDados
         private void InitializeComponent()
         {
             this.dtview = new System.Windows.Forms.DataGridView();
+            this.btsalvar = new System.Windows.Forms.Button();
+            this.btatualizar = new System.Windows.Forms.Button();
+            this.lbnome = new System.Windows.Forms.Label();
+            this.lbcpf = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtcpf = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dtview)).BeginInit();
             this.SuspendLayout();
             // 
             // dtview
             // 
             this.dtview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtview.Location = new System.Drawing.Point(12, 12);
+            this.dtview.Location = new System.Drawing.Point(12, 107);
             this.dtview.Name = "dtview";
-            this.dtview.Size = new System.Drawing.Size(776, 426);
+            this.dtview.Size = new System.Drawing.Size(776, 331);
             this.dtview.TabIndex = 0;
             this.dtview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtview_CellContentClick);
+            // 
+            // btsalvar
+            // 
+            this.btsalvar.Location = new System.Drawing.Point(518, 70);
+            this.btsalvar.Name = "btsalvar";
+            this.btsalvar.Size = new System.Drawing.Size(132, 31);
+            this.btsalvar.TabIndex = 1;
+            this.btsalvar.Text = "Salvar";
+            this.btsalvar.UseVisualStyleBackColor = true;
+            this.btsalvar.Click += new System.EventHandler(this.btsalvar_Click);
+            // 
+            // btatualizar
+            // 
+            this.btatualizar.Location = new System.Drawing.Point(656, 70);
+            this.btatualizar.Name = "btatualizar";
+            this.btatualizar.Size = new System.Drawing.Size(132, 31);
+            this.btatualizar.TabIndex = 2;
+            this.btatualizar.Text = "Atualizar";
+            this.btatualizar.UseVisualStyleBackColor = true;
+            this.btatualizar.Click += new System.EventHandler(this.btatualizar_Click);
+            // 
+            // lbnome
+            // 
+            this.lbnome.AutoSize = true;
+            this.lbnome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbnome.Location = new System.Drawing.Point(7, 25);
+            this.lbnome.Name = "lbnome";
+            this.lbnome.Size = new System.Drawing.Size(86, 25);
+            this.lbnome.TabIndex = 3;
+            this.lbnome.Text = "Nome: ";
+            // 
+            // lbcpf
+            // 
+            this.lbcpf.AutoSize = true;
+            this.lbcpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbcpf.Location = new System.Drawing.Point(7, 70);
+            this.lbcpf.Name = "lbcpf";
+            this.lbcpf.Size = new System.Drawing.Size(64, 25);
+            this.lbcpf.TabIndex = 4;
+            this.lbcpf.Text = "CPF:";
+            // 
+            // txtnome
+            // 
+            this.txtnome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnome.Location = new System.Drawing.Point(82, 22);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(706, 31);
+            this.txtnome.TabIndex = 5;
+            // 
+            // txtcpf
+            // 
+            this.txtcpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcpf.Location = new System.Drawing.Point(82, 67);
+            this.txtcpf.Mask = "000.000.000-00";
+            this.txtcpf.Name = "txtcpf";
+            this.txtcpf.Size = new System.Drawing.Size(166, 31);
+            this.txtcpf.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtcpf);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.lbcpf);
+            this.Controls.Add(this.lbnome);
+            this.Controls.Add(this.btatualizar);
+            this.Controls.Add(this.btsalvar);
             this.Controls.Add(this.dtview);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtview)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dtview;
+        private System.Windows.Forms.Button btsalvar;
+        private System.Windows.Forms.Button btatualizar;
+        private System.Windows.Forms.Label lbnome;
+        private System.Windows.Forms.Label lbcpf;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.MaskedTextBox txtcpf;
     }
 }
 
--- ConexaoBancoDeDados/Form1.cs
+++ ConexaoBancoDeDados/Form1.cs
@@ -47,5 +47,42 @@ namespace ConexaoBancoDeDados
         {
 
         }
+
+        private void btsalvar_Click(object sender, EventArgs e)
+        {
+            string sql = "INSERT INTO Clientes (nome,cpf)" + "VALUES ('" + txtnome.Text + "', '" + txtcpf.Text + "')";
+
+            SqlConnection conexaoNova = new SqlConnection("Server=179.108.180.209;Database=SistemaWendel;User Id=sa;Password=senac123;");
+            SqlCommand comando = new SqlCommand(sql, conexaoNova);
+            comando.CommandType = CommandType.Text;
+            conexaoNova.Open();
+            comando.ExecuteNonQuery();
+            conexaoNova.Close();
+        }
+
+        private void btatualizar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexaoNova = new SqlConnection("Server=179.108.180.209;Database=SistemaWendel;User Id=sa;Password=senac123;");
+            conexaoNova.Open();
+
+            //criando o select e o objeto de consulta
+            string sql = "SELECT * FROM Clientes";
+            SqlCommand cmd = new SqlCommand(sql, conexaoNova);
+            cmd.Connection = conexaoNova;
+            cmd.CommandText = sql;
+
+            // cria o dataadapter...
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+
+            // preenche o dataset...
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+
+            dtview.DataSource = dataSet;
+            dtview.DataMember = dataSet.Tables[0].TableName;
+
+            conexaoNova.Close();
+        }
     }
 }
